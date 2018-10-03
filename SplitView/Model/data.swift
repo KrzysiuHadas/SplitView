bb198e778064586969e7a559edeed6aa6036eeee
krzysztofHadas <hada10@gazeta.pl>
2018-10-03 17:51:49 +0200

    created dummy data

---
+++ SplitView/Model/data.swift
@@ -0,0 +1,29 @@
+//
+//  data.swift
+//  SplitView
+//
+//  Created by Krzysztof Hadaś on 03.10.18.
+//  Copyright © 2018 Krzysztof Hadaś. All rights reserved.
+//
+
+import UIKit
+
+// images
+let aurora = UIImage(named: "aurora")!
+let castle = UIImage(named: "castle")!
+let lake = UIImage(named: "lake")!
+let mountains = UIImage(named: "mountains")!
+let road = UIImage(named: "road")!
+let tree = UIImage(named: "tree")!
+let valley = UIImage(named: "valley")!
+
+
+// array of images
+let imageArray = [aurora, castle, lake, mountains, road, tree, valley]
+
+// names of images
+var namesArray = ["AURORA", "CASTLE", "LAKE", "MOUNTAINS", "ROAD", "TREE", "VALLEY"]
+
+// colors
+
+var colorsArray = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)]
